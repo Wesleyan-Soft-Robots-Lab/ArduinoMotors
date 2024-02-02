a8c59119629ce2116f553f1604a6999404b15d76
cdiazherrera <102235823+cdiazherrera@users.noreply.github.com>
2024-02-02 15:50:12 -0500

    Update sketch_oct15a.ino

--- PneumaticBoard/sketch_oct15a/sketch_oct15a.ino
+++ PneumaticBoard/sketch_oct15a/sketch_oct15a.ino
@@ -1,17 +1,42 @@
 
 int solenoidPin = 4; //output pin on the arduino board
 
+struct SolVals {
+  int open;
+  int closed;
+};
+
 
 
 void setup() {
   // put your setup code here, to run once:
+
 pinMode(solenoidPin, OUTPUT); //sets pin as output
 }
 
+//write a function that makes it controllaby high or low, input pow being percentage you want solenoid to close the valve
+
+
 void loop() {
+  // analogWrite(solenoidPin, 191);
   // put your main code here, to run repeatedly:
-digitalWrite(solenoidPin, HIGH); //switches solenoid to ON
-delay(1000); //wait 1 sec
-digitalWrite(solenoidPin, LOW); //switch it to OFF
-delay(1000); //wait 1 sec
+SolVals result = getTwoValues(90);
+
+
+analogWrite(solenoidPin, 255);
+delay(result.closed);
+analogWrite(solenoidPin, 0);
+delay(result.open);
+// delay(1000); //wait 1 sec
+// digitalWrite(solenoidPin, LOW); //switch it to OF`F
+// delay(1000); //wait 1 sec
+// analogWrite(solenoidPin, 260);
+}
+
+
+SolVals getTwoValues(int pow) {
+  SolVals result;
+  result.closed = pow*10;
+  result.open = 1000 - result.closed;
+  return result;
 }

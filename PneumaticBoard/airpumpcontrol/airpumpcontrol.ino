5226c52106abe9e3ea3898f63fa76db34bda98cb
Kevin Angulo Lezama <kevinjangulol@gmail.com>
2024-05-17 16:25:06 -0400

    Update airpumpcontrol.ino Individual control
    Added individual control to pairs of air valves.

--- PneumaticBoard/airpumpcontrol/airpumpcontrol.ino
+++ PneumaticBoard/airpumpcontrol/airpumpcontrol.ino
@@ -1,71 +1,98 @@
 // Code by Christian Diaz Herrera and Kevin Angulo Lezama
 
-//This is the most recent version of the pneumatic board control code that we have. this control the
-//rate at which the solenoids will be opening and closing per second. This controls airflow
+//This is the most recent version of the pneumatic board control. This allows for individual control of the dials and thus pairs of air valves. - Kevin Angulo Lezama
 
-//POTpin controls the dial
-int POTpin = A15;
-//solenoid pins control the output onto solenoids
-int solenoidPin = 12;
-int solenoidPin1 = 11;
-int solenoidPin2 = 10;
-int solenoidPin3 = 9;
-//values for which we can map open and closed rates(map) and value for which we see rate printed on screen(POT)
-int POTval, mapval; 
+int POTpin1 = A15;
+int POTpin2 = A14;
+int solenoidPin1 = 1;
+int solenoidPin2 = 2;
+int solenoidPin3 = 3;
+int solenoidPin4 = 4;
+int POTval1, POTval2, mapval1, mapval2; 
 
-//Structure for determining time open and time closed
 struct SolVals {
   int open;
   int closed;
 };
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(solenoidPin, OUTPUT);
-  pinMode(solenoidPin1, OUTPUT); //sets pin as output
-  pinMode(solenoidPin2, OUTPUT);
+  pinMode(solenoidPin1, OUTPUT);
+  pinMode(solenoidPin2, OUTPUT); //sets pin as output
   pinMode(solenoidPin3, OUTPUT);
+  pinMode(solenoidPin4, OUTPUT);
 }
 
 void loop() {
-  POTval = analogRead(POTpin);
-  mapval = map(POTval, 0, 1023, 0, 100);
+
+  // Getting the values from both dials
+  POTval1 = analogRead(POTpin1);
+  mapval1 = map(POTval1, 0, 1023, 0, 100);
+  
+  POTval2 = analogRead(POTpin2);
+  mapval2 = map(POTval2, 0, 1023, 0, 100);
   
   // Call getTwoValues to initialize result with the current mapval
-  SolVals result = getTwoValues(mapval);
+  SolVals result1 = getTwoValues(mapval1);
+  SolVals result2 = getTwoValues(mapval2);
   
   // POTval and mapval for debugging
-  Serial.print("POTval: ");
-  Serial.print(POTval);
-  Serial.print("\tMapval: ");
-  Serial.println(mapval);
+  Serial.print("POTval1: ");
+  Serial.print(POTval1);
+  Serial.print("\tMapval1: ");
+  Serial.println(mapval1);
   Serial.println();
-  // Open and Closed
-  Serial.print("Open: ");
-  Serial.print(result.open);
-  Serial.print("\tClosed: ");
-  Serial.println(result.closed);
+  Serial.print("POTval2: ");
+  Serial.print(POTval2);
+  Serial.print("\tMapval2: ");
+  Serial.println(mapval2);
+  Serial.println();
   
-  if (mapval >= 65) {
-    digitalWrite(solenoidPin, 255);
-    digitalWrite(solenoidPin1, 255);
-    digitalWrite(solenoidPin2, 255);
-    digitalWrite(solenoidPin3, 255);
-    Serial.println("Here");
+  // Open and Closed
+  Serial.print("Open1: ");
+  Serial.print(result1.open);
+  Serial.print("\tClosed1: ");
+  Serial.println(result1.closed);
+  Serial.println();
+  Serial.print("Open2: ");
+  Serial.print(result2.open);
+  Serial.print("\tClosed2: ");
+  Serial.println(result2.closed);
+  Serial.println();
+  
+  if (mapval1 >= 70) {
+    digitalWrite(solenoidPin1, HIGH); // Turn on solenoid.
+    digitalWrite(solenoidPin2, HIGH); // Turn on solenoid.
+    Serial.println("Solenoid 1 & 2 Here");
+  } else if (mapval1 <= 5){
+    digitalWrite(solenoidPin1, LOW); // Turn off solenoid.
+    digitalWrite(solenoidPin2, LOW); // Turn off solenoid.
   } else {
     // Use the result to control the solenoid
-    digitalWrite(solenoidPin1, 255); // Turn on solenoid.
-    digitalWrite(solenoidPin, 255); // Turn on solenoid.
-    digitalWrite(solenoidPin2, 255);
-    digitalWrite(solenoidPin3, 255);
-    //delay(1000); For debuggin purposes.
-    delay(result.open); // Delay for the time it should be open
-    digitalWrite(solenoidPin1, 0); // Turn off solenoid.
-    digitalWrite(solenoidPin, 0); // Turn off solenoid.
-    digitalWrite(solenoidPin2, 0);
-    digitalWrite(solenoidPin3, 0);
-    //delay(1000); For debbuging purposes. 
-    delay(result.closed); // Delay for the time it should be closed.
+    digitalWrite(solenoidPin1, HIGH); 
+    digitalWrite(solenoidPin2, HIGH); 
+    delay(result1.open); // Delay for the time it should be open
+    digitalWrite(solenoidPin1, LOW); 
+    digitalWrite(solenoidPin2, LOW);
+    delay(result1.closed); // Delay for the time it should be closed.
+  }  
+
+  if (mapval2 >= 70) {
+    digitalWrite(solenoidPin3, HIGH); // Turn on solenoid.
+    digitalWrite(solenoidPin4, HIGH); // Turn on solenoid.
+    Serial.println("Solenoid 3 & 4 Here");
+  } else if (mapval2 <= 5){
+    digitalWrite(solenoidPin3, LOW); // Turn off solenoid.
+    digitalWrite(solenoidPin4, LOW); // Turn off solenoid.
+  } else {
+    // Use the result to control the solenoid
+    digitalWrite(solenoidPin3, HIGH);
+    digitalWrite(solenoidPin4, HIGH);
+    delay(result2.open); // Delay for the time it should be open
+    digitalWrite(solenoidPin3, LOW);
+    digitalWrite(solenoidPin4, LOW);
+    delay(result2.closed); // Delay for the time it should be closed.
   }  
 }
 
